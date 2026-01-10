49a1ecf23fcc83e02f606ad2f28078d437ab5461
himanshu53929 <himanshuchandthakuri@gmail.com>
2026-01-11 00:52:13 +0545

    Update Path button added

--- route.js
+++ route.js
@@ -17,3 +17,20 @@ for (let i = 0; i < 150; i++) {
   s.style.transform = `scale(${0.2 + Math.random() * 0.8})`;
   backgroundDecor.appendChild(s);
 }
+
+const current_location = document.getElementById("current_location");
+const firstSelectedValue = current_location.value;
+current_location.addEventListener("change", () => {
+  firstSelectedValue = current_location.value;
+});
+
+const final_location = document.getElementById("final_location");
+const secondSelectedValue = final_location.value;
+final_location.addEventListener("change", () => {
+  secondSelectedValue = final_location.value;
+});
+
+const find_path_btn = document.getElementById("path_button");
+find_path_btn.addEventListener("click", () => {
+  //Transfer the first and secondvalue to the backend
+});
